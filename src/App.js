a023c00f94fe377786537fe54b9d088648c125b6
Pablo Jurado <pjurado85@gmail.com>
2018-05-05 12:30:50 -0500

    fix checkBoxesAround bug

--- src/App.js
+++ src/App.js
@@ -7,18 +7,17 @@ class App extends Component {
     super()
 
     this.state = {
+      renderCount: 0,
       gameOn: false,
       boardX: 10,
       boardY: 10,
       board: null
     };
 
-    this.prevBoard = this.makeBoard(this.state.boardX, this.state.boardY);
-    this.state.board = this.prevBoard;
+    this.state.board = this.makeBoard(this.state.boardX, this.state.boardY);
     this.intervalId = null;
 
     this.checkBoxesAround = this.checkBoxesAround.bind(this);
-    this.toggleLive = this.toggleLive.bind(this);
     this.makeBoard = this.makeBoard.bind(this);
     this.clickBox = this.clickBox.bind(this);
     this.reRender = this.reRender.bind(this);
@@ -41,10 +40,6 @@ class App extends Component {
     return board;
   }
 
-  toggleLive(x, y) {
-    this.prevBoard[x][y].live = !this.prevBoard[x][y].live;
-  }
-
   clickBox(x, y) {
     var newBoard = this.state.board;
     newBoard[x][y].live = !newBoard[x][y].live;
@@ -64,7 +59,32 @@ class App extends Component {
   }
 
   reRender() {
-    this.setState({ board: this.prevBoard });    
+    var currentBoard = this.state.board;
+    var nextBoard = deepClone(this.state.board);
+    var maxRow = this.state.boardX;
+    var maxCol = this.state.boardY;
+
+    for (let indexRow = 0; indexRow < maxRow; indexRow++) {
+      for (let indexCol = 0; indexCol < maxCol; indexCol++) {
+
+        var liveNeighbours = this.checkBoxesAround(indexRow, indexCol);
+        // Any live box with two or three live neighbours lives on to the next generation.
+        if (currentBoard[indexRow][indexCol].live) {
+          // Any live box with fewer than two live neighbours dies, as if caused by underpopulation.
+          // Any live box with more than three live neighbours dies, as if by overpopulation.
+          if (liveNeighbours < 2 || liveNeighbours > 3) {
+            nextBoard[indexRow][indexCol].live = false;
+          } 
+        } else {
+          // Any dead box with exactly three live neighbours becomes a live box, as if by reproduction.
+          if (liveNeighbours === 3) {
+            nextBoard[indexRow][indexCol].live = true;
+          }
+        }
+      }
+    } // end nested loop
+    var newRender = this.state.renderCount;
+    this.setState({ board: nextBoard, renderCount: newRender + 1 });
   }
 
   checkBoxesAround(boxX, boxY) {
@@ -74,6 +94,10 @@ class App extends Component {
 
     for (var x = boxX - 1; x < boxX  + 2; x++) {
       for (var y = boxY - 1; y < boxY + 2; y++) {
+        // avoid checking itself
+        if (boxX === x && boxY === y) {
+          continue;
+        }
         // check for limits outside the board
         if (x >= 0 && x < limitX && y >= 0 && y < limitY) {
           if (this.state.board[x][y].live) {
@@ -89,13 +113,12 @@ class App extends Component {
     return (
       <div className="App">
         <h4>Game of Life</h4>
+        <p>Generation: {this.state.renderCount}</p>
         <div className="board">
           <Board
             gameOn={this.state.gameOn}
             board={this.state.board}
-            toggleLive={this.toggleLive}
             clickBox={this.clickBox}
-            checkBoxesAround={this.checkBoxesAround}
           />
         </div>
         <br />
@@ -107,15 +130,17 @@ class App extends Component {
   }
 }
 
+function deepClone(element) {
+  return JSON.parse(JSON.stringify(element));
+}
+
 function Board(props) {
   var rows = props.board.map((row, idx) => {
     return (
       <Row key={idx}
         row={row}
         gameOn={props.gameOn}
-        toggleLive={props.toggleLive}
         clickBox={props.clickBox}
-        checkBoxesAround={props.checkBoxesAround}
       />
     )  
   })
@@ -128,9 +153,7 @@ function Row(props) {
       <Box 
         key={idx}
         gameOn={props.gameOn}
-        toggleLive={props.toggleLive}
         clickBox={props.clickBox}
-        checkBoxesAround={props.checkBoxesAround} 
         box={box} 
       />
     )
@@ -146,28 +169,6 @@ class Box extends Component {
     var y = box.y;
     var css = "box";
     if (box.live) css += " live";
-
-    var liveNeighbours = this.props.checkBoxesAround(x, y);
-
-    if (this.props.gameOn) {
-      // Any live box with two or three live neighbours lives on to the next generation.
-      if (this.props.box.live) {
-        // Any live box with fewer than two live neighbours dies, as if caused by underpopulation.
-        if (liveNeighbours < 2) {
-          this.props.toggleLive(x, y)
-        }
-        
-        // Any live box with more than three live neighbours dies, as if by overpopulation.
-        if (liveNeighbours > 3) {
-          this.props.toggleLive(x, y)
-        }  
-      } else {
-        // Any dead box with exactly three live neighbours becomes a live box, as if by reproduction.
-        if (liveNeighbours === 3) {
-          this.props.toggleLive(x, y)
-        }
-      }
-    }
       
     return <div className={css} onClick={this.props.clickBox.bind(null, x, y)} />;
   }
